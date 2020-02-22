cf5818e5fc9c92522102c1c55e25240bacb09bae
Tazi34 <pawelkas12345@gmail.com>
2020-02-22 02:57:27 +0100

    Improve car card tooltip

--- web-app/src/components/car/CarCard.js
+++ web-app/src/components/car/CarCard.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import { CardActions, Button, Card, CardContent, Typography, CardActionArea, CardMedia, List, ListItem, Container, Tooltip, Grid, makeStyles, Divider, } from '@material-ui/core';
-import carImage from '../../sampleCar.png'
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Divider, Grid, makeStyles, Tooltip, Typography } from '@material-ui/core';
+import { purple } from '@material-ui/core/colors';
 import AcUnitIcon from '@material-ui/icons/AcUnit';
 import Filter5Icon from '@material-ui/icons/Filter5';
-import { purple } from '@material-ui/core/colors';
+import React from 'react';
+import carImage from '../../sampleCar.png';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -16,9 +16,15 @@ const useStyles = makeStyles(theme => ({
         fontWeight:700,
         paddingTop:10,
     },
+    tooltip:{
+        color:purple,
+    },
     iconContainer:{
         paddingTop:10,
         marginBottom:-20,
+    },
+    icon:{
+        fontSize:35,
     }
 
 }));
@@ -43,13 +49,13 @@ function CarCard(props) {
                     <Divider/>
                     <Grid container spacing={1} direction="row" className={classes.iconContainer}>
                         <Grid item>
-                            <Tooltip title="Air conditioning" placement="top">
-                                <AcUnitIcon color="primary" fontSize="large" />
+                            <Tooltip className={classes.tooltip}  title="Air conditioning" placement="top">
+                                <AcUnitIcon className={classes.icon}  fontSize="large" />
                             </Tooltip>
                         </Grid>
                         <Grid item>
-                            <Tooltip title="Seats" placement="top">
-                                <Filter5Icon style={{ color: "green" }} fontSize="large" />
+                            <Tooltip className={classes.tooltip} title="Seats" placement="top">
+                                <Filter5Icon className={classes.icon}  style={{ color: "green" }} fontSize="large" />
                             </Tooltip>
                         </Grid>
                     </Grid>
