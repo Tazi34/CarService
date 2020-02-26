db869640aad389ce2afb0a7521f9e85f185eeb36
Tazi34 <pawelkas12345@gmail.com>
2020-02-26 15:15:01 +0100

    Add background for whole page

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,37 +1,40 @@
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-import React from 'react';
-import './App.css';
-import VisibleCarList from './components/car/VisibleCarList';
-import Layout from './components/UI/Layout'
-import { Route } from 'react-router-dom';
-import Home from './components/UI/Home';
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import React from "react";
+import "./App.css";
+import VisibleCarList from "./components/car/VisibleCarList";
+import Layout from "./components/UI/Layout";
+import { Route } from "react-router-dom";
+import ReservationForm from "./components/reservationForm/ReservationForm";
+import { purple } from "@material-ui/core/colors";
+import CssBaseline from "@material-ui/core/CssBaseline";
 const theme = createMuiTheme({
   palette: {
-
     primary: {
-      main: "#121212",
+      main: "#121212"
     },
 
     secondary: {
-      main:
-        "#01579b",
+      main: "#01579b"
+    },
+    background: {
+      default: "#303030"
     }
   },
-
   status: {
-    danger: 'orange',
-  },
+    danger: "orange"
+  }
 });
-
-
 
 function App(props) {
   return (
-    <MuiThemeProvider theme={theme} >
-      <Layout style={{width:"100%",height:"100%"}}>
-        <Route path="/" component={Home} />
-        <Route path="/cars" component={VisibleCarList} />
-      </Layout>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline>
+        {/*Needed to apply background for whole page */}
+        <Layout>
+          <Route exact path="/" component={ReservationForm} />
+          <Route path="/cars" component={VisibleCarList} />
+        </Layout>
+      </CssBaseline>
     </MuiThemeProvider>
   );
 }
