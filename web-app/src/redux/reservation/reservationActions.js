07cec16df466a68719a95cacd39c6728a776a90a
Tazi34 <pawelkas12345@gmail.com>
2020-02-27 02:46:40 +0100

    Add reservation info to redux

--- web-app/src/redux/reservation/reservationActions.js
+++ web-app/src/redux/reservation/reservationActions.js
@@ -4,12 +4,22 @@ export const SET_END_DATE = "SET_END_DATE";
 export const SET_START_SPOT = "SET_START_SPOT";
 export const SET_END_SPOT = "SET_END_SPOT";
 
+export const SET_START_CITY = "SET_START_CITY";
+export const SET_END_CITY = "SET_END_CITY";
 export function setStartDate(date) {
   return { type: SET_START_DATE, payload: { date: date } };
 }
 export function setEndDate(date) {
   return { type: SET_END_DATE, payload: { date: date } };
 }
+export const setStartCity = payload => ({
+  type: SET_START_CITY,
+  payload
+});
+export const setEndCity = payload => ({
+  type: SET_END_CITY,
+  payload
+});
 
 export function setEndSpot(spot) {
   return { type: SET_END_SPOT, payload: { spot: spot } };
--- web-app/src/redux/reservation/reservationReducer.js
+++ web-app/src/redux/reservation/reservationReducer.js
@@ -2,15 +2,33 @@ import {
   SET_START_DATE,
   SET_END_DATE,
   SET_END_SPOT,
-  SET_START_SPOT
+  SET_START_SPOT,
+  SET_START_CITY,
+  SET_END_CITY
 } from "./reservationActions";
 
 function reservations(
   state = {
     startDate: new Date(),
     endDate: new Date(),
-    startSpot: "",
-    endSport: ""
+    startSpot: {
+      id: -1,
+      name: ""
+    },
+    endSpot: {
+      id: -1,
+      name: ""
+    },
+    startCity: {
+      id: -1,
+      spots: [],
+      name: "Start city"
+    },
+    endCity: {
+      id: -1,
+      spots: [],
+      name: "End city"
+    }
   },
   action
 ) {
@@ -23,6 +41,10 @@ function reservations(
       return { ...state, startSpot: action.payload.spot };
     case SET_END_SPOT:
       return { ...state, endSpot: action.payload.spot };
+    case SET_START_CITY:
+      return { ...state, startCity: action.payload };
+    case SET_END_CITY:
+      return { ...state, endCity: action.payload };
     default:
       return state;
   }
