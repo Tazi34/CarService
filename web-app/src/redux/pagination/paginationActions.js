0deed9160e198f4dedbb74bb16af2b992fee4acb
Tazi34 <pawelkas12345@gmail.com>
2020-02-27 02:45:46 +0100

    Improve pagination redux

--- web-app/src/redux/pagination/paginationActions.js
+++ web-app/src/redux/pagination/paginationActions.js
@@ -1,72 +1,58 @@
-import axios from 'axios'
-import { receiveCars,receiveCarsError } from '../car/actions/carActions'
-import { buildUrl } from '../../urlAPI'
-export const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
-export const SET_TOTAL_PAGES = 'SET_TOTAL_PAGES'
-export const REQUEST_PAGE = 'REQUEST_PAGE'
-export const RECEIVE_PAGE = 'RECEIVE_PAGE'
-export const RESET_PAGINATION = 'RESET_PAGINATION'
+import axios from "axios";
+import { receiveCars, receiveCarsError } from "../car/carActions";
+import { buildUrl } from "../../urlAPI";
+export const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
+export const SET_TOTAL_PAGES = "SET_TOTAL_PAGES";
+export const REQUEST_PAGE = "REQUEST_PAGE";
+export const RECEIVE_PAGE = "RECEIVE_PAGE";
+export const RESET_PAGINATION = "RESET_PAGINATION";
 
-export const SET_SORT_FIELD = 'SET_SORT_FIELD'
-export const SET_SORT_ORDER = 'SET_SORT_ORDER'
-export const SET_SORTING = 'SET_SORTING'
+export const SET_SORT_FIELD = "SET_SORT_FIELD";
+export const SET_SORT_ORDER = "SET_SORT_ORDER";
+export const SET_SORTING = "SET_SORTING";
 
 export const SortFields = {
-    NONE:'NONE',
-}
+  NONE: "NONE"
+};
 export const SortOrders = {
-    ASC: 'ASC',
-    NOT_SORTED:'NOT_SORTED',
-    DESC: 'DESC',
-}
+  ASC: "ASC",
+  NOT_SORTED: "NOT_SORTED",
+  DESC: "DESC"
+};
 export function setSortField(sortField) {
-    return { type: SET_SORT_FIELD, payload:{sortField} }
+  return { type: SET_SORT_FIELD, payload: { sortField } };
 }
 
 export function setSortOrder(sortOrder) {
-    return { type: SET_SORT_ORDER, payload:{sortOrder} }
+  return { type: SET_SORT_ORDER, payload: { sortOrder } };
 }
-export function setSorting(sortField,sortOrder) {
-    return { type: SET_SORTING, payload:{sortField,sortOrder} }
-}
-
-export function setCurrentPage(page){
-    return {type:SET_CURRENT_PAGE,payload:{page}} 
+export function setSorting(sortField, sortOrder) {
+  return { type: SET_SORTING, payload: { sortField, sortOrder } };
 }
 
-export function setTotalePages(totalPages){
-    return {type:SET_TOTAL_PAGES
-        ,payload:{totalPages}}
+export function setCurrentPage(page) {
+  return { type: SET_CURRENT_PAGE, payload: { page } };
 }
 
-
-export function requestPage(endpoint,resultKey,page){
-    return {type:REQUEST_PAGE,
-        payload:{page},
-        meta:{endpoint,resultKey}
-    }
-}
-export function resetPagination(){
-    return {type:RESET_PAGINATION}
+export function setTotalePages(totalPages) {
+  return { type: SET_TOTAL_PAGES, payload: { totalPages } };
 }
 
-export function receivePage(page,results,totalPages){
-    return {type:RECEIVE_PAGE,payload:{page,results,totalPages}}
+export function requestPage(endpoint, resultKey, page) {
+  return {
+    type: REQUEST_PAGE,
+    payload: { page },
+    meta: { endpoint, resultKey }
+  };
+}
+export function resetPagination() {
+  return { type: RESET_PAGINATION };
 }
 
-export function createRequestPageActionCreator(endpoint,resultKey) {
-    return (page) => requestPage(endpoint,resultKey,page)
+export function receivePage(page, results, totalPages) {
+  return { type: RECEIVE_PAGE, payload: { page, results, totalPages } };
 }
 
-const requestCarPage = createRequestPageActionCreator('/cars','cars')
-
-export function fetchCarPage(pageNo = 0,sortField = null, sortOrder = null){
-    return function(dispatch){
-        dispatch(requestCarPage(pageNo))
-        return axios.get(buildUrl('/cars',pageNo,sortField,sortOrder))
-            .then(response => {
-                dispatch(receiveCars(response.data.content))
-                dispatch(receivePage(response.data.number,response.data.content,response.data.totalPages ))               
-            },error => dispatch(receiveCarsError(error)))
-    }
+export function createRequestPageActionCreator(endpoint, resultKey) {
+  return page => requestPage(endpoint, resultKey, page);
 }
--- web-app/src/redux/pagination/paginationReducer.js
+++ web-app/src/redux/pagination/paginationReducer.js
@@ -1,61 +1,71 @@
-import { REQUEST_PAGE, RECEIVE_PAGE, SET_CURRENT_PAGE, SET_SORT_ORDER, SET_SORT_FIELD, SortOrders, SET_SORTING, RESET_PAGINATION } from "./paginationActions";
+import {
+  REQUEST_PAGE,
+  RECEIVE_PAGE,
+  SET_CURRENT_PAGE,
+  SET_SORT_ORDER,
+  SET_SORT_FIELD,
+  SortOrders,
+  SET_SORTING,
+  RESET_PAGINATION
+} from "./paginationActions";
 import { combineReducers } from "redux";
 
-const { NOT_SORTED } = SortOrders
+const { NOT_SORTED } = SortOrders;
 
 const paginationInitialState = {
-    pages: {},
-    currentPage: 0,
-    totalPages: 0,
-}
+  pages: {},
+  currentPage: 0,
+  totalPages: 0
+};
 function pagination(state = paginationInitialState, action) {
-    switch (action.type) {
-        case REQUEST_PAGE:
-            return {
-                ...state,
-                pages: {
-                    [action.payload.page]: {
-                        ids: [],
-                        fetching: true
-                    }
-                }
-            }
-        case RECEIVE_PAGE:
-            return {
-                ...state,
-                currentPage: action.payload.page,
-                totalPages: action.payload.totalPages,
-                pages: {
-                    ...state.pages,
-                    [action.payload.page]: {
-                        ids: action.payload.results.map(item => item.id),
-                        fetching: false,
-                    }
-                }
-            }
-        case SET_CURRENT_PAGE:
-            return { ...state, currentPage: action.payload.page }
-        case RESET_PAGINATION:
-            return paginationInitialState
-        default:
-            return state
-    }
+  switch (action.type) {
+    case REQUEST_PAGE:
+      return {
+        ...state,
+        pages: {
+          ...state.pages,
+
+          [action.payload.page]: {
+            ids: [],
+            fetching: true
+          }
+        }
+      };
+    case RECEIVE_PAGE:
+      return {
+        ...state,
+        currentPage: action.payload.page,
+        totalPages: action.payload.totalPages,
+        pages: {
+          ...state.pages,
+          [action.payload.page]: {
+            ids: action.payload.results.map(item => item.id),
+            fetching: false
+          }
+        }
+      };
+    case SET_CURRENT_PAGE:
+      return { ...state, currentPage: action.payload.page };
+    case RESET_PAGINATION:
+      return paginationInitialState;
+    default:
+      return state;
+  }
 }
 function currentPage(state = -1, action) {
-    switch (action.type) {
-        case REQUEST_PAGE:
-            return action.payload.page
-        case SET_CURRENT_PAGE:
-            return { ...state, currentPage: action.payload.page }
+  switch (action.type) {
+    case REQUEST_PAGE:
+      return action.payload.page;
+    case SET_CURRENT_PAGE:
+      return { ...state, currentPage: action.payload.page };
 
-        default:
-            return state
-
-    }
+    default:
+      return state;
+  }
 }
 
 function totalPages(state = -1, action) {
-    return action.type == RECEIVE_PAGE ? action.payload.totalPages : state
+  return action.type == RECEIVE_PAGE ? action.payload.totalPages : state;
 }
 
 // function sortOrder(state = NOT_SORTED, action) {
@@ -76,18 +86,21 @@ function totalPages(state = -1, action) {
 // }
 
 function sorting(state = { order: NOT_SORTED, field: null }, action) {
-    switch (action.type) {
-        case SET_SORTING:
-            return  { ...state,order: action.payload.sortOrder, field: action.payload.sortField};
-        case SET_SORT_ORDER:
-            return { ...state, order: action.payload.sortOrder }
-        case SET_SORT_FIELD:
-            return { ...state, field: action.payload.sortField}
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case SET_SORTING:
+      return {
+        ...state,
+        order: action.payload.sortOrder,
+        field: action.payload.sortField
+      };
+    case SET_SORT_ORDER:
+      return { ...state, order: action.payload.sortOrder };
+    case SET_SORT_FIELD:
+      return { ...state, field: action.payload.sortField };
+    default:
+      return state;
+  }
 }
 
-export const paginationReducer = pagination
+export const paginationReducer = pagination;
 export const sortingReducer = sorting;
-
