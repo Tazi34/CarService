6c119cf2980a580d4b8c529ce0e1167ea386551e
Tazi34 <pawelkas12345@gmail.com>
2020-02-27 02:46:05 +0100

    Add cities to redux

---
+++ web-app/src/redux/city/cityActions.js
@@ -0,0 +1,44 @@
+import Axios from "axios";
+import { citiesURL } from "../../urlAPI";
+import { addSpots } from "./spot/spotActions";
+export const RECEIVE_CITIES_SUCCESS = "RECEIVE_CITIES_SUCCESS";
+export const RECEIVE_CITIES_ERROR = "RECEIVE_CITIES_ERROR";
+export const REQUEST_CITIES = "RECEIVE_CITIES";
+
+export const receiveCitiesSuccess = cities => {
+  return {
+    type: RECEIVE_CITIES_SUCCESS,
+    payload: { cities: cities }
+  };
+};
+
+export const receiveCitiesError = error => ({
+  type: RECEIVE_CITIES_ERROR,
+  payload: {
+    error: error
+  }
+});
+
+export const requestCities = () => {
+  return {
+    type: REQUEST_CITIES
+  };
+};
+
+export function fetchCities() {
+  return function(dispatch) {
+    dispatch(requestCities());
+    Axios.get(citiesURL).then(
+      response => {
+        dispatch(receiveCitiesSuccess(response.data));
+
+        dispatch(
+          addSpots(
+            response.data.map(city => city.spots).reduce((a, b) => a.concat(b))
+          )
+        );
+      },
+      error => dispatch(receiveCitiesError(error))
+    );
+  };
+}
---
+++ web-app/src/redux/city/cityReducer.js
@@ -0,0 +1,35 @@
+import {
+  RECEIVE_CITIES_SUCCESS,
+  REQUEST_CITIES,
+  RECEIVE_CITIES_ERROR
+} from "./cityActions";
+import { addSpots } from "./spot/spotActions";
+
+const initialState = {
+  items: {},
+  byId: [],
+  isFetching: false,
+  error: null,
+  fetched: false
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case REQUEST_CITIES:
+      return { ...state, isFetching: true };
+    case RECEIVE_CITIES_SUCCESS:
+      var _items = {};
+      //normalize data - whole items
+      action.payload.cities.forEach(city => (_items[city.id] = city));
+      return {
+        ...state,
+        //only id array
+        byId: [...state.byId, ...action.payload.cities.map(city => city.id)],
+        items: { ...state.items, ..._items }
+      };
+    case RECEIVE_CITIES_ERROR:
+      return { ...state, error: action.payload.error };
+    default:
+      return state;
+  }
+};
