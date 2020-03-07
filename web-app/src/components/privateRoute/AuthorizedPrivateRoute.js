e79390465768b30a188c1f0033fcfb06d99d3808
Tazi34 <pawelkas12345@gmail.com>
2020-03-07 02:02:39 +0100

    Change private route to work with authorization.

---
+++ web-app/src/components/privateRoute/AuthorizedPrivateRoute.js
@@ -0,0 +1,44 @@
+import { Route } from "react-router-dom";
+import { Redirect } from "react-router";
+import React from "react";
+
+export default function AuthorizedPrivateRoute({
+  component: Component,
+  user,
+  roles,
+  ...rest
+}) {
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        //not logged
+        if (!user) {
+          return (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: props.location }
+              }}
+            />
+          );
+        }
+        const userRoles = user.roles.map(role => role.name);
+        const isInRoles = userRoles.some(role => roles.includes(role));
+        //logged but not authorized
+        if (!isInRoles) {
+          return (
+            <Redirect
+              to={{
+                pathname: "/",
+                state: { from: props.location }
+              }}
+            />
+          );
+        }
+        //authorized
+        return <Component {...props} />;
+      }}
+    />
+  );
+}
