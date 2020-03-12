df9a6c653d163425088eafcfb8b080e5d682e08e
Tazi34 <p.kasjaniuk@student.mini.pw.edu.pl>
2020-03-12 03:21:46 +0100

    Add admin cars table dynamic pagination.

--- web-app/src/components/admin/cars/CarsTable.js
+++ web-app/src/components/admin/cars/CarsTable.js
@@ -9,15 +9,25 @@ import { compose } from "recompose";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import {
+  resetPagination,
+  setCurrentPage,
+  setPageSize
+} from "../../../redux/pagination/paginationActions";
 
 function mapStateToProps(state) {
   return {
-    cars: state.cars
+    cars: state.cars,
+    pagination: state.cars.pagination
   };
 }
 
 const mapDispatchToProps = {
-  fetchCarsPage: fetchCarsPage
+  setRowsPerPage: setPageSize,
+  fetchCarsPage: fetchCarsPage,
+  setPage: setCurrentPage,
+  resetPages: resetPagination
 };
 const styles = theme => ({
   table: {
@@ -30,45 +40,91 @@ const styles = theme => ({
 
 class CarsTable extends Component {
   componentDidMount() {
-    this.props.fetchCarsPage(0);
+    this.props.fetchCarsPage({
+      pageNo: 0,
+      size: this.props.pagination.pageSize
+    });
   }
+
+  handleChangePage = (event, newPage) => {
+    const pagination = this.props.pagination;
+    //IF NOT FETCHED
+    if (!pagination.pages[newPage]) {
+      this.props.fetchCarsPage({
+        pageNo: newPage,
+        size: this.props.pagination.pageSize
+      });
+    }
+    this.props.setPage(newPage);
+  };
+
+  handleChangeRowsPerPage = event => {
+    const rowsPerPage = parseInt(event.target.value, 10);
+    this.props.resetPages();
+    this.props.setRowsPerPage(rowsPerPage);
+    this.props.fetchCarsPage({
+      pageNo: 0,
+      size: rowsPerPage
+    });
+  };
 
   render() {
     const { classes } = this.props;
     const cars = this.props.cars.cars;
-    if (cars.isFetching) return <CircularProgress />;
-    const carItems = cars.byId.map(id => cars.items[id]);
+    const pagination = this.props.pagination;
+    if (
+      !pagination.pages[0] ||
+      pagination.pages[pagination.currentPage].fetching
+    ) {
+      return <CircularProgress />;
+    }
+    const currentCarsPage = pagination.pages[pagination.currentPage];
+    const carItems = currentCarsPage.ids.map(id => cars.items[id]);
     return (
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell className={classes.tableHeadRow}>Car</TableCell>
-              <TableCell className={classes.tableHeadRow} align="right">
-                Price &nbsp;(per day)
-              </TableCell>
-              <TableCell className={classes.tableHeadRow} align="right">
-                Seats
-              </TableCell>
-              <TableCell className={classes.tableHeadRow} align="right">
-                Year
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {carItems.map(row => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                  {row.model} {row.make}
-                </TableCell>
-                <TableCell align="right">{row.price}</TableCell>
-                <TableCell align="right">{row.seats}</TableCell>
-                <TableCell align="right">{row.year}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <div>
+        <Paper>
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell className={classes.tableHeadRow}>Car</TableCell>
+                  <TableCell className={classes.tableHeadRow} align="right">
+                    Price &nbsp;(per day)
+                  </TableCell>
+                  <TableCell className={classes.tableHeadRow} align="right">
+                    Seats
+                  </TableCell>
+                  <TableCell className={classes.tableHeadRow} align="right">
+                    Year
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {carItems.map(row => (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row">
+                      {row.model} {row.make}
+                    </TableCell>
+                    <TableCell align="right">{row.price}</TableCell>
+                    <TableCell align="right">{row.seats}</TableCell>
+                    <TableCell align="right">{row.year}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+              l
+            </Table>
+          </TableContainer>
+          <TablePagination
+            // rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={pagination.totalElements}
+            rowsPerPage={pagination.pageSize}
+            page={pagination.currentPage}
+            onChangePage={this.handleChangePage}
+            onChangeRowsPerPage={this.handleChangeRowsPerPage}
+          />
+        </Paper>
+      </div>
     );
   }
 }
--- web-app/src/redux/car/carAPIrequests.js
+++ web-app/src/redux/car/carAPIrequests.js
@@ -2,28 +2,46 @@ import axios from "axios";
 import { receiveCars, receiveCarsError } from "./carActions";
 import { buildUrl } from "../../urlAPI";
 import {
-  receivePage,
-  createRequestPageActionCreator
+  createRequestPageActionCreator,
+  receivePage
 } from "../pagination/paginationActions";
 
 const requestCarPage = createRequestPageActionCreator("/cars", "cars");
 
-export function fetchCarsPage(pageNo = 0, sortField = null, sortOrder = null) {
+export function fetchCarsPage({
+  pageNo = 0,
+  sortField = null,
+  sortOrder = null,
+  size = 10
+}) {
   return function(dispatch) {
     dispatch(requestCarPage(pageNo));
-    return axios.get(buildUrl("/cars", pageNo, sortField, sortOrder)).then(
-      response => {
-        dispatch(receiveCars(response.data.content));
-        dispatch(
-          receivePage(
-            response.data.number,
-            response.data.content,
-            response.data.totalPages
-          )
-        );
-      },
-      error => dispatch(receiveCarsError(error))
-    );
+    return axios
+      .get(
+        buildUrl({
+          endpoint: "/cars",
+          pageNo: pageNo,
+          sortField: sortField,
+          sortOrder: sortOrder,
+          size: size
+        })
+      )
+      .then(
+        response => {
+          dispatch(receiveCars(response.data.content));
+
+          dispatch(
+            receivePage(
+              response.data.number,
+              response.data.content,
+              response.data.totalPages,
+              response.data.totalElements,
+              response.data.size
+            )
+          );
+        },
+        error => dispatch(receiveCarsError(error))
+      );
   };
 }
 
@@ -36,13 +54,21 @@ export function fetchAvailableCarsPage(
   spotID = null
 ) {
   return function(dispatch) {
-    var fromISO = new Date(from).toISOString();
-    var toISO = new Date(to).toISOString();
+    const fromISO = new Date(from).toISOString();
+    const toISO = new Date(to).toISOString();
 
     dispatch(requestCarPage(pageNo));
     return axios
       .get(
-        buildUrl("/cars", pageNo, sortField, sortOrder, fromISO, toISO, spotID)
+        buildUrl({
+          endpoint: "/cars",
+          pageNo,
+          sortField,
+          sortOrder,
+          from: fromISO,
+          to: toISO,
+          spot: spotID
+        })
       )
       .then(
         response => {
