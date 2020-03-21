b4c1d6f65bf69764aa55199962a10bd39d24560f
Tazi34 <p.kasjaniuk@student.mini.pw.edu.pl>
2020-03-21 16:16:18 +0100

    Add CarSummary

---
+++ web-app/src/components/reservations/reservationSummary/carSummary/CarSummary.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import sampleCar from "../../../../images/cars/carWhiteBackGround.jpg";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import { CarDetailsTable } from "./CarDetailsTable";
+
+const useStyles = makeStyles({
+  image: {
+    width: "100%",
+    padding: 5
+  }
+});
+export const CarSummary = props => {
+  const classes = useStyles();
+  const car = props.car;
+  return (
+    <Grid
+      container
+      direction={"row"}
+      justify={"space-evenly"}
+      alignItems={"center"}
+    >
+      <Grid item xs={12} sm={6}>
+        <CarDetailsTable car={car} />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <img
+          alt={`${car.make} ${car.model}`}
+          src={sampleCar}
+          className={classes.image}
+        ></img>
+      </Grid>
+    </Grid>
+  );
+};
