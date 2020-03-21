5f01647833f816b4b6f330d4d90f5e2118347a1c
Tazi34 <p.kasjaniuk@student.mini.pw.edu.pl>
2020-03-21 16:11:45 +0100

    Add CarDetailsTable

---
+++ web-app/src/components/reservations/reservationSummary/carSummary/CarDetailsTable.js
@@ -0,0 +1,29 @@
+import React from "react";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Table from "@material-ui/core/Table";
+import { TableBody, TableContainer } from "@material-ui/core";
+import { NameValueTableRow } from "../../../UI/NameValueTableRow";
+
+const useStyles = makeStyles({
+  root: {
+    padding: 10,
+    textAlign: "right"
+  }
+});
+
+export const CarDetailsTable = props => {
+  const classes = useStyles();
+  const car = props.car;
+  return (
+    <TableContainer className={classes.root}>
+      <Table>
+        <TableBody>
+          <NameValueTableRow name={"Car"} value={`${car.make} ${car.model}`} />
+          <NameValueTableRow name={"Seats"} value={car.seats} />
+          <NameValueTableRow name={"Doors"} value={car.doors} />
+          <NameValueTableRow name={"Year"} value={car.year} />
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
