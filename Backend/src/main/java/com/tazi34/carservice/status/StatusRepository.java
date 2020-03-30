a4166ec246d65b9fbdcf8ca6209e893d8155d8c4
Tazi34 <p.kasjaniuk@student.mini.pw.edu.pl>
2020-03-31 01:13:36 +0200

    Add checkIfAvailable test in CarService

--- Backend/src/main/java/com/tazi34/carservice/status/StatusRepository.java
+++ Backend/src/main/java/com/tazi34/carservice/status/StatusRepository.java
@@ -2,6 +2,7 @@ package com.tazi34.carservice.status;
 
 import com.tazi34.carservice.car.Car;
 import com.tazi34.carservice.clientInfo.ClientInfo;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -11,7 +12,7 @@ import java.util.List;
 public interface StatusRepository extends PagingAndSortingRepository<Status, Long>, JpaSpecificationExecutor<Status> {
     List<Status> deleteAllByCar(Car car);
 
-    List<Status> findByClientInfo(ClientInfo clientInfo);
+    List<Status> findByClientInfo(ClientInfo clientInfo, Sort sort);
 
 
 }
--- Backend/src/main/java/com/tazi34/carservice/status/StatusService.java
+++ Backend/src/main/java/com/tazi34/carservice/status/StatusService.java
@@ -13,6 +13,7 @@ import com.tazi34.carservice.exceptions.BadRequestException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -90,7 +91,7 @@ public class StatusService {
         return !from.before(currentDateToCompare) && !from.after(to);
     }
     public List<Status> getClientsStatuses(ClientInfo clientInfo){
-        return statusRepository.findByClientInfo(clientInfo);
+        return statusRepository.findByClientInfo(clientInfo, Sort.by("dateFrom").descending());
     }
     public List<Status> findCollidingBookedStatuses(Date from, Date to, long carId) {
         return statusRepository.findAll(StatusSpecifications.collidesWithDateSpan(from, to).and(StatusSpecifications.byCarId(carId).and(StatusSpecifications.isType(StatusType.BOOKED))));
