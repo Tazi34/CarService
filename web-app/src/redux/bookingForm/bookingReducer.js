7685a70812f1af61d0539b6b14910641800965c0
Tazi34 <p.kasjaniuk@student.mini.pw.edu.pl>
2020-03-31 14:53:15 +0200

    Add missing booking redux files

---
+++ web-app/src/redux/bookingForm/bookingReducer.js
@@ -0,0 +1,136 @@
+import {
+  CONFIRM_DATE_LOCATION,
+  POST_RESERVATION,
+  POST_RESERVATION_ERROR,
+  POST_RESERVATION_SUCCESS,
+  SELECT_CAR,
+  SET_END_CITY,
+  SET_END_DATE,
+  SET_END_SPOT,
+  SET_START_CITY,
+  SET_START_DATE,
+  SET_START_SPOT
+} from "./bookingActions";
+import { combineReducers } from "redux";
+import clientDetailsReducer from "../clientDetails/clientDetailsReducer";
+import {
+  REQUEST_TOTAL_RESERVATION_PRICE,
+  REQUEST_TOTAL_RESERVATION_PRICE_ERROR,
+  REQUEST_TOTAL_RESERVATION_PRICE_SUCCESS
+} from "./priceActions";
+
+function reservation(
+  state = {
+    startDate: new Date(),
+    endDate: new Date(new Date().getTime() + 86400000),
+    startSpot: {
+      id: -1,
+      name: ""
+    },
+    endSpot: {
+      id: -1,
+      name: ""
+    },
+    startCity: {
+      selected: false,
+      item: {
+        id: -1,
+        spots: [],
+        name: "Start city"
+      }
+    },
+    endCity: {
+      selected: false,
+      item: {
+        id: -1,
+        spots: [],
+        name: "Start city"
+      }
+    },
+    car: {
+      item: {},
+      selected: false
+    },
+    totalPrice: {
+      price: 0,
+      fetching: false,
+      error: null
+    },
+    status: {
+      dateLocationPicked: false,
+      pending: false,
+      error: null
+    }
+  },
+  action
+) {
+  //TODO split into different reducers
+  switch (action.type) {
+    case SET_START_DATE:
+      return { ...state, startDate: action.payload.date };
+    case SET_END_DATE:
+      return { ...state, endDate: action.payload.date };
+    case SET_START_SPOT:
+      return { ...state, startSpot: action.payload.spot };
+    case SET_END_SPOT:
+      return { ...state, endSpot: action.payload.spot };
+    case SET_START_CITY:
+      return { ...state, startCity: { item: action.payload, selected: true } };
+    case SET_END_CITY:
+      return { ...state, endCity: { item: action.payload, selected: true } };
+    case SELECT_CAR:
+      return { ...state, car: { item: action.payload.car, selected: true } };
+    case POST_RESERVATION:
+      return { ...state, status: { ...state.status, pending: true } };
+    case POST_RESERVATION_SUCCESS:
+      return { ...state, status: { ...state.status, pending: false } };
+    case POST_RESERVATION_ERROR:
+      return {
+        ...state,
+        status: { pending: false, error: action.payload.error }
+      };
+    case CONFIRM_DATE_LOCATION:
+      return {
+        ...state,
+        status: { ...state.status, dateLocationPicked: true }
+      };
+    case REQUEST_TOTAL_RESERVATION_PRICE: {
+      return {
+        ...state,
+        totalPrice: {
+          price: 0,
+          fetching: true,
+          error: null
+        }
+      };
+    }
+    case REQUEST_TOTAL_RESERVATION_PRICE_SUCCESS: {
+      return {
+        ...state,
+        totalPrice: {
+          price: action.payload.price,
+          fetching: false,
+          error: null
+        }
+      };
+    }
+    case REQUEST_TOTAL_RESERVATION_PRICE_ERROR: {
+      return {
+        ...state,
+        totalPrice: {
+          price: -1,
+          fetching: false,
+          error: action.payload.error
+        }
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+const bookingReducer = combineReducers({
+  reservation,
+  clientDetails: clientDetailsReducer
+});
+export default bookingReducer;
