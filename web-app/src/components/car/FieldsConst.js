4659cd6a8c57368611fc51071ff16d769b6a71d7
Tazi34 <p.kasjaniuk@student.mini.pw.edu.pl>
2020-03-31 19:33:44 +0200

    Format sortCarsOrders - git issue

--- web-app/src/components/car/FieldsConst.js
+++ web-app/src/components/car/FieldsConst.js
@@ -1,15 +1,14 @@
-
 export const SortCarsOrderFields = {
-    BY_PRICE:{
-        display:"Price",
-        value:"price"
-    },
-    BY_SEATS:{
-        display:"Seats",
-        value:"seats",
-    },
-    BY_YEAR:{
-        display:"Year",
-        value:"year",
-    } 
-}
+  BY_PRICE: {
+    display: "Price",
+    value: "price"
+  },
+  BY_SEATS: {
+    display: "Seats",
+    value: "seats"
+  },
+  BY_YEAR: {
+    display: "Year",
+    value: "year"
+  }
+};
