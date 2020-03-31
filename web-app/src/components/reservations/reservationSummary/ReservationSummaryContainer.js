37c702db0afbfa0c9990a3dcb668b7a7d15570c5
Tazi34 <p.kasjaniuk@student.mini.pw.edu.pl>
2020-03-31 14:51:51 +0200

    Add reservation summary redux container

---
+++ web-app/src/components/reservations/reservationSummary/ReservationSummaryContainer.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchBookingPrice } from "../../../redux/bookingForm/priceActions";
+import { CircularProgress } from "@material-ui/core";
+import ReservationSummary from "./ReservationSummary";
+import { Redirect } from "react-router";
+
+function mapStateToProps(state) {
+  return {
+    reservationData: state.bookingForm.reservation
+  };
+}
+
+const mapDispatchToProps = {
+  fetchReservationTotalPrice: fetchBookingPrice
+};
+
+class ReservationSummaryContainer extends Component {
+  componentDidMount() {
+    const { car, startDate, endDate } = this.props.reservationData;
+    this.props.fetchReservationTotalPrice(car.item.id, startDate, endDate);
+  }
+
+  render() {
+    const { reservationData } = this.props;
+    if (
+      !reservationData.status.dateLocationPicked ||
+      !reservationData.car.selected
+    ) {
+      return <Redirect to={"/"} />;
+    }
+    const { fetching: priceFetching, error } = reservationData.totalPrice;
+    if (priceFetching || error) {
+      return <CircularProgress />;
+    }
+    return <ReservationSummary reservation={reservationData} />;
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ReservationSummaryContainer);
