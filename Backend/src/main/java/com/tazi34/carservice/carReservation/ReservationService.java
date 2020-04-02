a5ea656eeb72d810a60ac3f520afe54ebfa6e8fd
Tazi34 <p.kasjaniuk@student.mini.pw.edu.pl>
2020-04-02 04:13:18 +0200

    Change non existing client info handling in getUserReservationByEmail

--- Backend/src/main/java/com/tazi34/carservice/carReservation/ReservationService.java
+++ Backend/src/main/java/com/tazi34/carservice/carReservation/ReservationService.java
@@ -1,12 +1,14 @@
 package com.tazi34.carservice.carReservation;
 
+import com.tazi34.carservice.car.Car;
+import com.tazi34.carservice.clientInfo.ClientInfo;
 import com.tazi34.carservice.clientInfo.ClientInfoService;
 import com.tazi34.carservice.exceptions.BadRequestException;
+import com.tazi34.carservice.exceptions.notFound.ResourceNotFoundException;
 import com.tazi34.carservice.status.Status;
 import com.tazi34.carservice.status.StatusService;
 import com.tazi34.carservice.status.StatusType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -25,48 +27,55 @@ public class ReservationService {
         this.statusService = statusService;
         this.clientInfoService = clientInfoService;
     }
+
     public void setReservationMapper(ReservationMapper reservationMapper) {
         this.reservationMapper = reservationMapper;
     }
 
-
     public List<ReservationInfo> getUserReservationsByEmail(String email) {
-        if (email == null) throw new RuntimeException("Email can't be null");
+        ClientInfo clientInfo;
 
-        var clientInfo = clientInfoService.getClientInfoByEmail(email);
-        if (clientInfo == null) throw new ResourceNotFoundException("Client info not found");
+        try {
+            clientInfo = clientInfoService.getClientInfoByEmail(email);
+        }catch(ResourceNotFoundException clientNotFoundException){
+            return new ArrayList<>();
+        }
+
         List<Status> clientsStatuses = statusService.getClientsStatuses(clientInfo);
-        if (clientsStatuses.isEmpty()) return new ArrayList<ReservationInfo>();
+
+        if (clientsStatuses.isEmpty()) {
+            return new ArrayList<>();
+        }
+
         return mapStatusesToReservations(clientsStatuses);
     }
-    private List<ReservationInfo> mapStatusesToReservations(List<Status> statuses) {
-        return statuses.stream().map(status -> reservationMapper.mapStatusToReservation(status)).collect(Collectors.toList());
-    }
-    public ReservationInfo getReservationInfo(long id){
-        Status status = null;
-        //TODO  create new exception class
-        var exceptionMessage = "Reservation not found.";
-        try {
-            status = statusService.getStatus(id);
+
+    public ReservationInfo getReservationInfo(long id) {
+        Status status = statusService.getStatus(id);
+
+        if (!isBooked(status)) {
+            throw new ResourceNotFoundException(Car.class);
         }
-        catch(ResourceNotFoundException exception){
-            throw new ResourceNotFoundException(exceptionMessage);
-        }
-        if (!isBooked(status))
-            throw new ResourceNotFoundException(exceptionMessage);
 
         return reservationMapper.mapStatusToReservation(status);
     }
-    public void cancelReservation(Long id){
+
+    public void cancelReservation(Long id) {
         var status = statusService.getStatus(id);
-        if(!isBooked(status)) throw new BadRequestException("Wrong status type. Must be of type BOOKED");
+
+        if (!isBooked(status)) {
+            throw new BadRequestException("Wrong status type. Must be of type BOOKED");
+        }
         status.setType(StatusType.BOOKINGCANCELED);
         statusService.updateStatus(status);
     }
-    private boolean isBooked(Status status){
+
+    private boolean isBooked(Status status) {
         var type = status.getType();
         return type == StatusType.BOOKED;
     }
 
-
+    private List<ReservationInfo> mapStatusesToReservations(List<Status> statuses) {
+        return statuses.stream().map(status -> reservationMapper.mapStatusToReservation(status)).collect(Collectors.toList());
+    }
 }
