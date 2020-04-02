e6e2491f964c5b662deb594dd142a114b21b7408
Tazi34 <p.kasjaniuk@student.mini.pw.edu.pl>
2020-04-02 04:08:58 +0200

    Refactor GetClientInfoByEmail tests

--- Backend/src/test/java/tests/clientInfo/service/GetClientInfoByEmailTests.java
+++ Backend/src/test/java/tests/clientInfo/service/GetClientInfoByEmailTests.java
@@ -1,26 +1,16 @@
 package tests.clientInfo.service;
 
-import com.tazi34.carservice.car.CarService;
 import com.tazi34.carservice.clientInfo.ClientInfo;
 import com.tazi34.carservice.clientInfo.ClientInfoRepository;
 import com.tazi34.carservice.clientInfo.ClientInfoService;
-import com.tazi34.carservice.clientInfo.address.AddressRepository;
+import com.tazi34.carservice.exceptions.notFound.ResourceNotFoundException;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -28,50 +18,38 @@ import java.util.Arrays;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
 public class GetClientInfoByEmailTests {
 
-    @Configuration
-    static class ClientInfoServiceTestContextConfiguration {
-        @Bean
-        public ClientInfoRepository clientInfoRepository() {
-            return Mockito.mock(ClientInfoRepository.class);
-        }
-        @Bean
-        public AddressRepository addressRepository() {
-            return Mockito.mock(AddressRepository.class);
-        }
-
-        @Bean ClientInfoService clientInfoService(){
-            return new ClientInfoService(clientInfoRepository(),addressRepository());
-        }
-    }
-
-    @Autowired
+    @Mock
     private ClientInfoRepository repository;
 
-    @Autowired
+    @InjectMocks
     private ClientInfoService clientInfoService;
 
-    @Test
-    public void shouldReturnNullIfNonExistingEmail() {
+    @Test(expected = ResourceNotFoundException.class)
+    public void givenNonExistingEmail_throw404() {
         //GIVEN
         var email = "nonexisting@mail.com";
         when(repository.findAllByEmail(email)).thenReturn(new ArrayList<>());
+
         //WHEN
-        var clientInfo = clientInfoService.getClientInfoByEmail(email);
-        Assert.assertNull(clientInfo);
+        clientInfoService.getClientInfoByEmail(email);
+
+        //THEN
     }
 
     @Test
-    public void shouldReturnClientInfo() {
+    public void givenExistingMail_returnClientInfo() {
         //GIVEN
         var email = "existing@mail.com";
         var client = Mockito.mock(ClientInfo.class);
 
         when(repository.findAllByEmail(email)).thenReturn(Arrays.asList(client));
+
         //WHEN
         var clientInfo = clientInfoService.getClientInfoByEmail(email);
+
+        //THEN
         Assert.assertEquals(clientInfo,client);
     }
 }
